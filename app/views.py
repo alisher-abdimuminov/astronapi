891331ff60aea12a48b96b3fc816aff2b6a5848a
Ali <alisher.abdimuminov.2005@gmail.com>
2025-03-24 10:40:46 +0500

    bug fix

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Counter
 
-
+@csrf_exempt
 def count(request: HttpRequest, date: str):
     counter = Counter.objects.filter(date=date)
     if counter:
